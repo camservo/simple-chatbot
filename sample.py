56e40cb930b39db5140bdbcd8b2f586336a67b3c
Cameron Jeffries <cameron.jeffries@terazo.com>
2024-03-24 09:16:43 -0500

    updating openai module to be a class

--- sample.py
+++ sample.py
@@ -3,22 +3,24 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-import app.ask_questions as ask_questions
+import app.openai as openai
 from app.audio import SpeechToTextConverter, TextToSpeechConverter
+from app.openai import OpenAiQuery
 
 if __name__ == "__main__":
     load_dotenv()
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     tts_converter = TextToSpeechConverter(client)
     stt_converter = SpeechToTextConverter(client)
+    query_engine = OpenAiQuery(client)
     tts_converter.default_renderer = "chatgpt"
     n = 1
     for i in range(n):
         question = stt_converter.convert()
         if question:
             print("Question: ", question)
-            for sentence in ask_questions.get_openai_responses_in_sentences(
-                client, question
+            for sentence in query_engine.get_openai_responses_in_sentences(
+                question=question
             ):
-                print("Sentence: ", sentence)
+                print("Response sentence: ", sentence)
                 tts_converter.convert(text=sentence)
